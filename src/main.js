9a903a9b44110fef64f002d1eff98837546d54e8
Jonas de Fraga Fonseca <jonasfonseca@MacBook-Pro-Pessoal.local>
2020-07-20 12:14:51 -0300

    Upgrade lodash from Yarn

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-alert('Antes de bisbilhotar tente fazer os exercícios antes, please!!!')
+class App {
+    constructor() {
+        this.repositories = [];
+        this.formEl = document.getElementById('repo-form');
+        this.registerHandlers();
+    }
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+    addRepository(event) {
+        event.preventDefault();
+        this.repositories.push({
+            name: 'rocketseat.com.br',
+            description: 'Tire a sua ideia do papel e dê vida à sua startup.',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url: 'http://github.com/jonasffonseca/rocketseat_starter_es06_conceitos_desafio'
+        });
+        console.log(this.repositories);
+    }
+}
+
+new App();
